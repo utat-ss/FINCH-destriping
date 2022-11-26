e0c4d4e5658e4e3199491d6226eb74c2b8c32f07
Rishit Dagli <rishit.dagli@gmail.com>
2022-11-26 12:55:17 -0500

    top-level docstrings for mixup

---
+++ destriping/augmentation_pipeline/mixup.py
@@ -0,0 +1,29 @@
+"""
+Author: [@Rishit-Dagli](https://github.com/Rishit-dagli)
+"""
+"""
+_mixup_ is a *domain-agnostic* data augmentation technique proposed in [mixup: Beyond Empirical Risk Minimization](https://arxiv.org/abs/1710.09412)
+by Zhang et al. It's implemented with the following formulas:
+
+![](https://i.ibb.co/DRyHYww/image.png)
+(Note that the lambda values are values with the [0, 1] range and are sampled from the
+[Beta distribution](https://en.wikipedia.org/wiki/Beta_distribution).)
+
+The technique is quite systematically named. We are literally mixing up the features and
+their corresponding labels. Implementation-wise it's simple. Neural networks are prone
+to [memorizing corrupt labels](https://arxiv.org/abs/1611.03530). mixup relaxes this by
+combining different features with one another (same happens for the labels too) so that
+a network does not get overconfident about the relationship between the features and
+their labels.
+
+mixup is specifically useful when we are not sure about selecting a set of augmentation
+transforms for a given dataset, medical imaging datasets, for example. mixup can be
+extended to a variety of data modalities such as computer vision, naturallanguage
+processing, speech, and so on.
+
+"""
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers
