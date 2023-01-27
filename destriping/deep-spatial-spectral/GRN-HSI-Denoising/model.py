bed396e6fde5383aef8c57260a8bec93a257cfd4
pun49030 <73437143+pun49030@users.noreply.github.com>
2023-01-27 07:00:17 +0000

    import tensorflow 1 instead of tensorflow 2

--- destriping/deep-spatial-spectral/GRN-HSI-Denoising/model.py
+++ destriping/deep-spatial-spectral/GRN-HSI-Denoising/model.py
@@ -1,9 +1,8 @@
-
-
-
 # external
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
 
 
 def spatialBlock(input_tensor):
@@ -11,18 +10,18 @@ def spatialBlock(input_tensor):
 
     channels = in_channels // 2
 
-    theta = tf.keras.layers.Conv2D(channels, 1, padding="valid")(input_tensor)
+    theta = tf.layers.conv2d(input_tensor, channels, 1, padding="valid")
     theta = tf.reshape(
         theta,
         shape=[-1, tf.shape(input_tensor)[1] * tf.shape(input_tensor)[2], channels],
     )
 
-    phi = tf.keras.layers.Conv2D(channels, 1, padding="valid")(input_tensor)
+    phi = tf.layers.conv2d(input_tensor, channels, 1, padding="valid")
     phi = tf.reshape(
         phi, shape=[-1, tf.shape(input_tensor)[1] * tf.shape(input_tensor)[2], channels]
     )
 
-    g = tf.keras.layers.Conv2D(channels, 1, padding="valid")(input_tensor)
+    g = tf.layers.conv2d(input_tensor, channels, 1, padding="valid")
     g = tf.reshape(
         g, shape=[-1, tf.shape(input_tensor)[1] * tf.shape(input_tensor)[2], channels]
     )
@@ -45,7 +44,7 @@ def spatialBlock(input_tensor):
         y, shape=[-1, tf.shape(input_tensor)[1], tf.shape(input_tensor)[2], channels]
     )
 
-    spatial_out = tf.keras.layers.Conv2D(in_channels, 1, padding="valid")(F_s)
+    spatial_out = tf.layers.conv2d(F_s, in_channels, 1, padding="valid")
 
     return spatial_out
 
@@ -57,10 +56,10 @@ def GloRe(X):
     N = imput_chancel // 4
     C = imput_chancel // 2
 
-    B = tf.keras.layers.Conv2D(N, 1, padding="valid")(X)
+    B = tf.layers.conv2d(X, N, 1, padding="valid")
     B = tf.reshape(B, [inputs_shape[0], -1, N])  # [B, H*W, N]
 
-    x_reduced = tf.keras.layers.Conv2D(C, 1, padding="valid")(X)
+    x_reduced = tf.layers.conv2d(X, C, 1, padding="valid")
     x_reduced = tf.reshape(x_reduced, [inputs_shape[0], -1, C])  # [B,  H*W, C]
     x_reduced = tf.transpose(x_reduced, perm=[0, 2, 1])  # [B, C, H*W]
 
@@ -74,14 +73,14 @@ def GloRe(X):
     v = tf.expand_dims(v, axis=1)  # [B, 1, C, N]
 
     def GCN(Vnode, nodeN, mid_chancel):
-        net = tf.keras.layers.Conv2D(N, 1, padding="valid")(Vnode)  # [B, 1, C, N]
+        net = tf.layers.conv2d(Vnode, N, 1, padding="valid")  # [B, 1, C, N]
 
         net = Vnode + net  # (I-Ag)V
         net = tf.nn.relu(net)
 
         net = tf.transpose(net, perm=[0, 3, 1, 2])  # [B, N, 1, C]
 
-        net = tf.keras.layers.Conv2D(mid_chancel, 1, padding="valid")(net)  # [B, N, 1, C]
+        net = tf.layers.conv2d(net, mid_chancel, 1, padding="valid")  # [B, N, 1, C]
 
         return net
 
@@ -98,7 +97,7 @@ def GloRe(X):
     y = tf.reshape(
         y, [inputs_shape[0], inputs_shape[1], inputs_shape[2], C]
     )  # [B, H, W, C]
-    x_res = tf.keras.layers.Conv2D(imput_chancel, 1, padding="valid")(y)
+    x_res = tf.layers.conv2d(y, imput_chancel, 1, padding="valid")
 
     return x_res
 
@@ -108,22 +107,22 @@ def blockE(_input):
 
     input_tensor = _input
 
-    conv1 = tf.keras.layers.Conv2D(
-        channels, 3, padding="SAME", activation=tf.nn.relu
-    )(input_tensor)
+    conv1 = tf.layers.conv2d(
+        input_tensor, channels, 3, padding="SAME", activation=tf.nn.relu
+    )
 
     #    tmp = tf.concat([input_tensor, conv1],-1)
     tmp = tf.add(input_tensor, conv1)
 
-    conv2 = tf.keras.layers.Conv2D(channels, 3, padding="SAME", activation=tf.nn.relu) (tmp)
+    conv2 = tf.layers.conv2d(tmp, channels, 3, padding="SAME", activation=tf.nn.relu)
 
     tmp = tf.add(tmp, conv2)
     #    tmp = tf.concat([input_tensor, conv1, conv2],-1)
-    conv3 = tf.keras.layers.Conv2D(channels, 3, padding="SAME", activation=tf.nn.relu) (tmp)
+    conv3 = tf.layers.conv2d(tmp, channels, 3, padding="SAME", activation=tf.nn.relu)
 
     #    tmp = tf.concat([input_tensor, conv1, conv2, conv3],-1)
     tmp = tf.add(tmp, conv3)
-    fuse = tf.keras.layers.Conv2D(channels, 1, padding="SAME")(tmp)
+    fuse = tf.layers.conv2d(tmp, channels, 1, padding="SAME")
 
     fuse = fuse + GloRe(fuse)
 
@@ -135,100 +134,102 @@ def blockD(_input):
 
     input_tensor = _input + spatialBlock(_input)
 
-    conv1 = tf.keras.layers.Conv2D(
-        channels, 3, padding="SAME", activation=tf.nn.relu
-    )(input_tensor)
+    conv1 = tf.layers.conv2d(
+        input_tensor, channels, 3, padding="SAME", activation=tf.nn.relu
+    )
 
     #    tmp = tf.concat([input_tensor, conv1],-1)
     tmp = tf.add(input_tensor, conv1)
-    conv2 = tf.keras.layers.Conv2D(channels, 3, padding="SAME", activation=tf.nn.relu)(tmp)
+    conv2 = tf.layers.conv2d(tmp, channels, 3, padding="SAME", activation=tf.nn.relu)
 
     #    tmp = tf.concat([input_tensor, conv1, conv2],-1)
     tmp = tf.add(tmp, conv2)
-    conv3 = tf.keras.layers.Conv2D(channels, 3, padding="SAME", activation=tf.nn.relu)(tmp)
+    conv3 = tf.layers.conv2d(tmp, channels, 3, padding="SAME", activation=tf.nn.relu)
 
     #    tmp = tf.concat([input_tensor, conv1, conv2, conv3],-1)
     tmp = tf.add(input_tensor, conv3)
-    fuse = tf.keras.layers.Conv2D(channels, 1, padding="SAME")(tmp)
+    fuse = tf.layers.conv2d(tmp, channels, 1, padding="SAME")
 
     return fuse + _input
 
 
 def Inference(images, channels=64):
     inchannels = images.get_shape().as_list()[-1]
-    print("Test")
-    print("Type of Images: ", type(images))
-    print("Shape of Images: ", images.get_shape().as_list())
-    with tf.compat.v1.variable_scope("UNet"):
 
-        with tf.compat.v1.variable_scope("basic"):
-            basic = tf.keras.layers.Conv2D(channels, 3, padding="SAME")(images)
-            basic1 = tf.keras.layers.Conv2D(channels, 3, padding="SAME")(basic)
+    with tf.variable_scope("UNet", reuse=tf.AUTO_REUSE):
 
-        with tf.compat.v1.variable_scope("encoder0"):
+        with tf.variable_scope("basic"):
+            basic = tf.layers.conv2d(images, channels, 3, padding="SAME")
+            basic1 = tf.layers.conv2d(basic, channels, 3, padding="SAME")
+
+        with tf.variable_scope("encoder0"):
             encode0 = blockE(basic1)
-            donw0 = tf.keras.layers.Conv2D(
-                channels, 3, strides=2, padding="SAME", activation=tf.nn.relu
-            )(encode0)
+            donw0 = tf.layers.conv2d(
+                encode0, channels, 3, strides=2, padding="SAME", activation=tf.nn.relu
+            )
 
-        with tf.compat.v1.variable_scope("encoder1"):
+        with tf.variable_scope("encoder1"):
             encode1 = blockE(donw0)
-            donw1 = tf.keras.layers.Conv2D(
-                channels, 3, strides=2, padding="SAME", activation=tf.nn.relu
-            )(encode1)
+            donw1 = tf.layers.conv2d(
+                encode1, channels, 3, strides=2, padding="SAME", activation=tf.nn.relu
+            )
 
-        with tf.compat.v1.variable_scope("encoder2"):
+        with tf.variable_scope("encoder2"):
             encode2 = blockE(donw1)
-            donw2 = tf.keras.layers.Conv2D(
-                channels, 3, strides=2, padding="SAME", activation=tf.nn.relu
-            )(encode2)
+            donw2 = tf.layers.conv2d(
+                encode2, channels, 3, strides=2, padding="SAME", activation=tf.nn.relu
+            )
 
-        with tf.compat.v1.variable_scope("middle"):
+        with tf.variable_scope("middle"):
 
             media_end = blockE(donw2)
 
-        with tf.compat.v1.variable_scope("decoder2"):
-            Deblock2 = tf.image.resize(media_end, size=[tf.shape(encode2)[1], tf.shape(encode2)[2]], method='bilinear')
+        with tf.variable_scope("decoder2"):
+            Deblock2 = tf.image.resize_images(
+                media_end, [tf.shape(encode2)[1], tf.shape(encode2)[2]], method=1
+            )
             Deblock2 = tf.concat([Deblock2, encode2], -1)
-            Deblock2 = tf.keras.layers.Conv2D(
-                channels, 1, padding="SAME", activation=tf.nn.relu
-            )(Deblock2)
+            Deblock2 = tf.layers.conv2d(
+                Deblock2, channels, 1, padding="SAME", activation=tf.nn.relu
+            )
             Deblock2 = blockD(Deblock2)
 
-        with tf.compat.v1.variable_scope("decoder1"):
-            Deblock1 = tf.image.resize(Deblock2, [tf.shape(encode1)[1], tf.shape(encode1)[2]], method='bilinear')
+        with tf.variable_scope("decoder1"):
+            Deblock1 = tf.image.resize_images(
+                Deblock2, [tf.shape(encode1)[1], tf.shape(encode1)[2]], method=1
+            )
             Deblock1 = tf.concat([Deblock1, encode1], -1)
-            Deblock1 = tf.keras.layers.Conv2D(
-                channels, 1, padding="SAME", activation=tf.nn.relu
-            )(Deblock1)
+            Deblock1 = tf.layers.conv2d(
+                Deblock1, channels, 1, padding="SAME", activation=tf.nn.relu
+            )
             Deblock1 = blockD(Deblock1)
 
-        with tf.compat.v1.variable_scope("decoder0"):
-            Deblock0 = tf.image.resize(Deblock1, [tf.shape(encode1)[1], tf.shape(encode1)[2]], method='bilinear')
-            Deblock0 = tf.concat([Deblock0, encode0, basic1], axis=-1)
-            Deblock0 = tf.keras.layers.Conv2D(
-                channels, 1, padding="SAME", activation=tf.nn.relu
-            )(Deblock0)
+        with tf.variable_scope("decoder0"):
+            Deblock0 = tf.image.resize_images(
+                Deblock1, [tf.shape(encode0)[1], tf.shape(encode0)[2]], method=1
+            )
+            Deblock0 = tf.concat([Deblock0, encode0, basic1], -1)
+            Deblock0 = tf.layers.conv2d(
+                Deblock0, channels, 1, padding="SAME", activation=tf.nn.relu
+            )
             Deblock0 = blockD(Deblock0)
 
-        with tf.compat.v1.variable_scope("reconstruct"):
+        with tf.variable_scope("reconstruct"):
             decoding_end = Deblock0 + basic
-            res = tf.keras.layers.Conv2D(inchannels, 3, padding="SAME")(decoding_end)
+            res = tf.layers.conv2d(decoding_end, inchannels, 3, padding="SAME")
             out = images + res
 
     return out
 
 
 if __name__ == "__main__":
-    ##tf.reset_default_graph()
-    # change placeholder
-    input_x = tf.keras.Input(shape=(101, 101, 1))
-    
+    tf.reset_default_graph()
+    input_x = tf.placeholder(tf.float32, [10, 101, 101, 1])
     #    out = spatialBlock(input_x)
     out = Inference(input_x)
 
     print(input_x.get_shape().as_list())
-    print("Pass line 234")
+
     all_vars = tf.trainable_variables()
     print(
         "Total parameters' number: %d"
