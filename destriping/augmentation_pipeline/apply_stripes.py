a4d083940ffb849c19ee800cae4fc897436bc80e
John Ma <jpaulma23@gmail.com>
2024-01-02 12:51:29 -0500

    Cleaned up some parameters and edited the binary_stripes and gaussian stripes methods to include varying lengths and clusters

--- destriping/augmentation_pipeline/apply_stripes.py
+++ destriping/augmentation_pipeline/apply_stripes.py
@@ -27,35 +27,74 @@ def _check_configs(configs):
         'clusters': False,
         'vary_len': False,
         'by_layers': True,
-        'stripe_intensity': 0.7,
+        'stripe_frequency': 1,
+        'noise_range_scaling': 1,
         'max_clusters': 10,
         'bit': 16
     }
     if configs is None:
         return default_values
-    # Update config with default values for missing keys
+    # Update configs with default values for missing keys
     for key, default_value in default_values.items():
         configs[key] = configs.get(key, default_value)
     return configs
 
-def _binary_stripe(data, stripe_frequency, noise_range_scaling):
+def _binary_stripe(data, configs):
+
+    '''
+    TODO
+    REDO THIS:
+    Issues:
+        - col lines selects the lines we need
+        - 
+    '''
     # Stripe is there or it isn't.
+    dims = data.shape
+
+    noise_range_scaling = configs['noise_range_scaling']
+
+    if configs['vary_len'] is True:
+        start_index, end_index = _choose_len_indices(dims)
+
+    # lines to edit
+    if configs['by_layer'] is False:
+        # same lines for each layer to edit
+        # TODO: EDIT SELECT LINES!!!!!!!
+        col_lines = _select_lines(dims,configs)
+
     for i in range(data.shape[2]):
+        if configs['by_layer'] is True:
+            # selects lines for one layer
+            col_lines = _select_lines(dims,configs)
+        
+        # Generate a random array of indices based on this col lines
+        # TODO: change the output of select lines to only output ND-arrays
+        random_indices = np.random.choice(len(col_lines), size=len(col_lines), replace=False)
+
+        # Split the array based on the random indices
+        part1 = data[random_indices[:len(col_lines)//2]]
+        part2 = data[random_indices[len(col_lines)//2:]]
+
+        # set the stripe intensity
         stripe_intensity=(np.max(data[:,:,i])-np.min(data[:,:,i]))*noise_range_scaling
+
         # Half frequency positive. Half frequency negative.
 
         # Positive Stripes
-        is_noise=np.random.rand(data.shape[1])<stripe_frequency/2
-        noise=np.tile(np.where(is_noise, stripe_intensity, 0), (data.shape[0],1))
+        #is_noise=np.random.rand(data.shape[1])< stripe_frequency/2
+
+        noise=np.tile(np.where(part1, stripe_intensity, 0), (data.shape[0],1))
 
         # Negative Stripes
-        is_noise=np.random.rand(data.shape[1])<stripe_frequency/2
-        noise+=np.tile(np.where(is_noise, -stripe_intensity, 0), (data.shape[0],1))
-        data[:,:,i]+=noise
+        #is_noise=np.random.rand(data.shape[1])< stripe_frequency/2
+
+        noise+=np.tile(np.where(part2, -stripe_intensity, 0), (data.shape[0],1))
+
+        data[start_index:end_index,col_lines,i]+=noise
 
     return data
 
-def _gaussian_stripe(data, noise_level):
+def _gaussian_stripe(data, configs):
     # Reference: https://www.mdpi.com/2072-4292/6/11/11082, Section 2.1
     # Direction: invariant per scene or within a short time span (Parallel to direction of motion)
         # Noise is additive
@@ -63,39 +102,31 @@ def _gaussian_stripe(data, noise_level):
         # Dark current varies Gaussianly
         #  0 Mean, 1 Standard Deviation. Rescaled later
         # four different striping scenarios, scales are standard deviations of 0.1%, 0.5%, 1% and 5% of individual band's range
+        # pick a random length
+    dims = data.shape
+    noise_level = configs['noise_level'] # [0, 1)
+
+    if configs['vary_len'] is True:
+        start_index, end_index = _choose_len_indices(dims)
+
+    # select columns
+    if configs['by_layer']is True:
+        col_lines = _select_lines(dims,configs)
+
+
     for i in range(data.shape[2]):
+        if configs['by_layer'] is False:
+            col_lines = _select_lines(dims,configs)
+
         mean=0
         range_value=np.max(data[:,:,i])-np.min(data[:,:,i])
-        std_dev=np.array([0.001, 0.005, 0.01, 0.05])*range_value
-        # Choose std_dev
-        deviation=std_dev[noise_level]
-        noise=np.random.normal(mean, deviation, data.shape[1])
-        noise=np.tile(noise, (data.shape[0],1))
-        data[:,:,i]+=noise
+        std_dev=noise_level*range_value
+
+        noise=np.random.normal(mean, std_dev, len(col_lines))
+        data[start_index:end_index,col_lines,i]+= noise
     return data
 
-def _vary_len(striped_data, col_striped, stripe_type, stripe_intensity):
-    """
-    Helper function
-    Takes a column of a frame of the data cube and 
-    makes a varied length for it.
-    
-    Two cases: one for which we get a 240x240 frame
-    and the other for the entire data_cube
-
-    args: 
-        striped_data: the 3D hyperspectral cube or one frame of it
-        col_striped: the column to change
-
-    returns:
-        cube
-    """
-    # random length and random position
-    # we can pick where it can be
-    dims = striped_data.shape
-
-
-    
+def _choose_len_indices(dims):
     # pick a random length
     length = np.random.uniform(0, dims[1])
 
@@ -105,9 +136,8 @@ def _vary_len(striped_data, col_striped, stripe_type, stripe_intensity):
     # Calculate the start and end indices of the selected portion
     start_index = int(max(position - length / 2, 0))
     end_index = int(min(position + length / 2, dims[0]))
+    return start_index, end_index
 
-
-    return
 
 def _add_snp_noise(cube, salt_prob = 0.02, pepper_prob=0.02):
     """
@@ -149,12 +179,16 @@ def _add_gaussian_noise(cube,bit, mean_percent =0.05, std_percent=0.1 ):
 
 
 
-def _select_lines(dims, clusters, r, max_clusters, v = 0.05):
+def _select_lines(dims, configs):
     """
+    Helper function, 
+    Selects lines to add noise to
     args:
     dims: tuple of the data cube.
     clusters: boolean
     r: ratio of columns to select
+    max_clusters: int
+    v: stripe intensity???
 
     returns:
     List of int, representing the lines to return
@@ -162,6 +196,10 @@ def _select_lines(dims, clusters, r, max_clusters, v = 0.05):
 
 
     cols_striped = set()
+    max_clusters = configs['max_cluster']
+    clusters = configs['clusters']
+
+    #TODO figure out stripe intensity
 
     # if there are clusters we would want to find areas to cluster these values
     # number of clusters are usual uniform, in addition to the number of stripes
@@ -174,76 +212,54 @@ def _select_lines(dims, clusters, r, max_clusters, v = 0.05):
         # for each cluster center
         for cols in cluster_cols:
             # add number of stripes per cluster
-            num_stripes = np.round(v*np.random.uniform(0, dims[1]*r, size=1)//num_clusters).astype(np.int64)
+            # max number of stripes must be less than a certain number
+            num_stripes = np.round(np.random.uniform(0, dims[1], size=1)//num_clusters).astype(np.int64)
             # for each "pivot" generate a cluster ie, positions of each striped column
             # may need to change how this works  bc of how intensity works (if intensity high, becomes closer together
+            num_stripes = np.clip(num_stripes, 0, dims[1]-1)
+
             cluster = np.round(np.random.uniform(cols - num_stripes//2 ,cols + num_stripes//2 , num_stripes)).astype(np.int64)
             cluster = np.clip(cluster, 0, dims[1]-1)
             cols_striped.add(cluster)
         # make sure each line is non-repeating in the list of lines
-        cols_striped = set(cols_striped)
+        cols_striped = np.array(list(set(cols_striped)))
     else:
-        num_lines =np.round(np.random.uniform(0, (dims[1]-1)*r, 1)).astype(np.int64)
+        # stripe frequency here
+        num_lines =np.round(np.random.uniform(0, (dims[1]-1), 1)).astype(np.int64)
         cols_striped =np.round(np.random.uniform(0, dims[1]-1, num_lines)).astype(np.int64)
 
     return cols_striped
 
 
-def add_stripes(datacube, config=None):
+def add_stripes(datacube, configs=None):
     """
     Adds stripes to the data cube depending on the parameters
 
     Args:
-    gauss_noise: boolean, if True add in noise to the datacube
-    snp_noise: boolean, if True add in salt and pepper noise
-    vary_len: boolean, if True adds multiplicative stripes into the data
-    empty: boolean, if True adds empty strips into the data
-    clusters: boolean, if True adds clustered stripes into the data
-    max_clusters: int, max number of clusters to generate
-    bit: int, the number of bits the data is represented in
+    datacube: np array
+    configs: dictionary
     
     Returns:
     striped_data: data cube with stripes added to it
     """
-    data_cube = datacube
 
     striped_data = copy.deepcopy(
-        data_cube
+        datacube
     )
-    dims = data_cube.shape
+    # sets default values if some configs values do not exist
+    configs = _check_configs(configs)
 
+    # depending on the stripe types
+    if configs['stripe_type'] == 'gaussian':
+        _gaussian_stripe(striped_data, 0 ,configs)
+    elif configs['striped_type'] == 'binary':
+        _binary_stripe(striped_data, 1, 1 ,configs)
 
-    config = _check_configs(config)
-
-    # each layer selects different lines
-    if config['by_layers'] is True:
-        for i in range(0, dims[2]):
-            # select a random amount of lines
-            col_lines = _select_lines(dims,config['stripe_type'] ,config['clusters'], config['r'], config['max_clusters'])
-            # vary the length of these columns
-            if config['vary_len']:
-                _vary_len(striped_data, col_lines, config['stripe_type'], config['stripe_intensity'])
-            else:
-                striped_data[:, col_lines, i] = 0
-
-
-    # each layer selects the same lines
-    elif config['by_layers'] is False:
-        # select a random amount of lines
-        col_lines = _select_lines(dims, config['clusters'], config['r'], config['max_clusters'])
-        # vary the length of these columns
-        if config['vary_len']:
-            _vary_len(striped_data, col_lines, config['stripe_type'], config['stripe_intensity'])
-        elif config['stripe_type'] == 'gaussian':
-            _gaussian_stripe(striped_data, 0)
-        elif config['striped_type'] == 'binary':
-            _binary_stripe(striped_data, 1, 1)
-            
 
     # add noise to the frame
-    if config['gauss_noise']:
-        striped_data = _add_gaussian_noise(striped_data, config['bit'])
-    if config['snp_noise']:
-        striped_data = _add_snp_noise(striped_data, config['bit'])
+    if configs['gauss_noise']:
+        striped_data = _add_gaussian_noise(striped_data, configs['bit'])
+    if configs['snp_noise']:
+        striped_data = _add_snp_noise(striped_data, configs['bit'])
 
     return striped_data
