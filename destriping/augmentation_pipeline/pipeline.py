1ec9c038a4c1908e07bd052eca3bfa7b977360ba
Dav Vrat Chadha <95514650+DavVratChadha@users.noreply.github.com>
2024-01-13 15:45:54 -0500

    Dav vrat cutmix (#14)
    * modified default args, output generation, propagated size & alpha to generate_augmented_images
    
    * Fixed print statement

--- destriping/augmentation_pipeline/pipeline.py
+++ destriping/augmentation_pipeline/pipeline.py
@@ -1,9 +1,11 @@
 import sys
+import os
+import shutil
 
 sys.path.append("..")
 from data.loader import HyperSpectralCube, AugmentedDataset
-from augmentation_pipeline.cutmix_mixup import generate_augmented_images, visualize_augmentations, cutmix_augmentation, mixup_augmentation
-import random
+# from augmentation_pipeline.cutmix_mixup import generate_augmented_images, visualize_augmentations, cutmix_augmentation, mixup_augmentation
+# import random
 import argparse
 
 def main(args):
@@ -17,19 +19,25 @@ def main(args):
     )
 
     output_directory = args.output_directory
+
+    #remove existing output directory and create a new one
+    if os.path.exists(output_directory):
+        shutil.rmtree(output_directory)
+    os.makedirs(output_directory)
+    
     for i in range(aug.__len__()):
-        output_path = f"{output_directory}/{i}.png"
+        output_path = f"{output_directory}/{i + 1}.png"
         aug.plot(i, save_to=output_path)
-    print(f"Saved augmented dataset to {output_path}")
+    print(f"Saved augmented dataset to {output_directory}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate and save augmented images from a hypercube.")
     parser.add_argument("--hypercube_path", type=str, help="Path to the hypercube file.")
     parser.add_argument("--hyperspectral_label", type=str, default="paviaU", help="The label of the hyperspectral cube in the .mat file.")
     parser.add_argument("--alpha", type=float, default=0.5, help="Alpha value for AugmentedDataset.")
-    parser.add_argument("--size", type=int, default=32, help="Size parameter for AugmentedDataset.")
-    parser.add_argument("--mixup_images", type=int, default=100, help="Number of mixup images for AugmentedDataset.")
-    parser.add_argument("--cutmix_images", type=int, default=100, help="Number of cutmix images for AugmentedDataset.")
+    parser.add_argument("--size", type=int, default=128, help="Size parameter for AugmentedDataset.")
+    parser.add_argument("--mixup_images", type=int, default=15, help="Number of mixup images for AugmentedDataset.")
+    parser.add_argument("--cutmix_images", type=int, default=15, help="Number of cutmix images for AugmentedDataset.")
     parser.add_argument("--output_directory", type=str, default="output_images", help="Directory to save augmented images.")
 
     args = parser.parse_args()
